0e0646a6eddff4f86332d21e1ae74aa88c0c9083
Clare MacAdie <clare@macadie.co.uk>
2023-01-03 13:10:02 +0000

    Used alphabet rather than ascii codes

--- rot13Cipher.js
+++ rot13Cipher.js
@@ -1,82 +1,23 @@
-/*
-P
-Create a function that takes a string and returns a new string with each
-letter rotated by 13 spaces.
-
-E
-Case-sensitive
-Only letters should be rotated
-
-D
-Input - string
-Return - string
-
-A
-Create a helper function to determine if a character is lower, uppper, or neither case, determineCase
-Create a helper function that takes a lowercase letter and returns rotated by 13 lowercase letter, rot13Lower
-Same for uppercase letter, rot13Upper
-
-=Main function=
-Create newStr as empty string to store rotated characters
-Iterate from 0 to length of input string, i
-  Use determineCase to find case of string[i] char
-    if lowercase, use rot13Lower to get new char, append to newStr
-    if uppercase, use rot13Upper to get new char, append to newStr
-    otherwise append char to newStr
-Return newStr
-*/
-
-const LOWER_ASCII_START = 'a'.charCodeAt();
-const LOWER_ASCII_MID = LOWER_ASCII_START + 13;
-const LOWER_ASCII_FINISH = 'z'.charCodeAt();
-const UPPER_ASCII_START = 'A'.charCodeAt();
-const UPPER_ASCII_MID = UPPER_ASCII_START + 13;
-const UPPER_ASCII_FINISH = 'Z'.charCodeAt();
-
-function determineCase(char) {
-  let charAscii = char.charCodeAt();
-  if (charAscii >= LOWER_ASCII_START && charAscii <= LOWER_ASCII_FINISH) {
-    return 'lower';
-  } else if (charAscii >= UPPER_ASCII_START && charAscii <= UPPER_ASCII_FINISH) {
-    return 'upper'
-  } else {
-    return 'neither';
-  }
-}
-
-function rot13Lower(char) {
-  const ROTATION_SHIFT = 13;
-  let charAscii = char.charCodeAt();
-  if (charAscii < LOWER_ASCII_MID) {
-    return String.fromCharCode(charAscii + ROTATION_SHIFT);
-  } else {
-    return String.fromCharCode(charAscii - ROTATION_SHIFT);
-  }
-}
-
-function rot13Upper(char) {
-  const ROTATION_SHIFT = 13;
-  let charAscii = char.charCodeAt();
-  if (charAscii < UPPER_ASCII_MID) {
-    return String.fromCharCode(charAscii + ROTATION_SHIFT);
-  } else {
-    return String.fromCharCode(charAscii - ROTATION_SHIFT);
-  }
-}
-
 function rot13(string) {
+  const LOWER_ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+  const UPPER_ALPHABET = LOWER_ALPHABET.toUpperCase();
+  const ALPHABET_LENGTH = LOWER_ALPHABET.length;
+  const ROTATION_SHIFT = 13;
   let newStr = '';
+
   for (let idx = 0; idx < string.length; idx++) {
     let char = string[idx];
-    let charCase = determineCase(char);
-    switch (charCase) {
-    case 'lower':
-      char = rot13Lower(char);
-      break;
-    case 'upper':
-      char = rot13Upper(char);
-      break;
+    
+    if (LOWER_ALPHABET.includes(char)) {
+      let currentIndex = LOWER_ALPHABET.indexOf(char);
+      let newIndex = (currentIndex + ROTATION_SHIFT) % ALPHABET_LENGTH;
+      char = LOWER_ALPHABET[newIndex];
+    } else if (UPPER_ALPHABET.includes(char)) {
+      let currentIndex = UPPER_ALPHABET.indexOf(char);
+      let newIndex = (currentIndex + ROTATION_SHIFT) % ALPHABET_LENGTH;
+      char = UPPER_ALPHABET[newIndex];
     }
+
     newStr += char;
   }
 
