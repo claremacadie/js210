e9a8991c92322693b42096f5c9a5382e1525aa61
Clare MacAdie <clare@macadie.co.uk>
2023-01-03 13:19:55 +0000

    Added helper function

--- rot13Cipher.js
+++ rot13Cipher.js
@@ -1,21 +1,22 @@
+function rot13Char(char, alphabet) {
+  const ROTATION_SHIFT = 13;
+  let currentIndex = alphabet.indexOf(char);
+  let newIndex = (currentIndex + ROTATION_SHIFT) % alphabet.length;
+  return alphabet[newIndex];
+}
+
 function rot13(string) {
   const LOWER_ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
   const UPPER_ALPHABET = LOWER_ALPHABET.toUpperCase();
-  const ALPHABET_LENGTH = LOWER_ALPHABET.length;
-  const ROTATION_SHIFT = 13;
   let newStr = '';
 
   for (let idx = 0; idx < string.length; idx++) {
     let char = string[idx];
     
     if (LOWER_ALPHABET.includes(char)) {
-      let currentIndex = LOWER_ALPHABET.indexOf(char);
-      let newIndex = (currentIndex + ROTATION_SHIFT) % ALPHABET_LENGTH;
-      char = LOWER_ALPHABET[newIndex];
+      char = rot13Char(char, LOWER_ALPHABET)
     } else if (UPPER_ALPHABET.includes(char)) {
-      let currentIndex = UPPER_ALPHABET.indexOf(char);
-      let newIndex = (currentIndex + ROTATION_SHIFT) % ALPHABET_LENGTH;
-      char = UPPER_ALPHABET[newIndex];
+      char = rot13Char(char, UPPER_ALPHABET)
     }
 
     newStr += char;
@@ -23,16 +24,3 @@ function rot13(string) {
 
   return newStr;
 }
-
-console.log(rot13('Teachers open the door, but you must enter by yourself.'));
-// logs:
-// Grnpuref bcra gur qbbe, ohg lbh zhfg ragre ol lbhefrys.
-
-console.log(rot13(rot13('Teachers open the door, but you must enter by yourself.')));
-// logs:
-// Teachers open the door, but you must enter by yourself.
-const foobar4 = "[\\]^_`";
-console.log(rot13(foobar4) === foobar4);
-
-const foobar5 = "{|}~";
-console.log(rot13(foobar5) === foobar5);
